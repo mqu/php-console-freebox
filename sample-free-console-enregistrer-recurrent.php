67aeb4b5b1ea5f04c634f19c46a33c41d3c8a01e
Marc Quinton <m.quinton@gmail.com>
2011-04-13 07:21:00 +0200

    test et démonstration de l'API : EnregistrementRecurrent.

---
+++ sample-free-console-enregistrer-recurrent.php
@@ -0,0 +1,40 @@
+<?php
+
+error_reporting(E_ALL);
+
+error_reporting(E_ALL);
+
+require_once('config.php');
+require_once('InfosChaine.php');
+require_once('ConsoleMagneto.php');
+
+require_once('Enregistrement.php');
+require_once('EnregistrementRecurrent.php');
+require_once('EnregistrementFreebox.php');
+
+$magneto = new ConsoleMagneto();
+$magneto->login($config['user'], $config['passwd']);
+
+# $date = '12/03/2011';
+$date = date('d/m/Y');
+
+$enreg = new Enregistrement();
+$enreg->date    = $date;
+$enreg->heure   = 19;
+$enreg->minutes = 57;
+$enreg->duree   = 36;
+
+$enreg->repeat   = array(1,2,3);
+
+$enreg->chaine   = 'France 2';   # si PB accent, remplacer par expression reg (ex : 'Public S.*nat')
+$enreg->qualite  = 'standard';   # auto, standard, bas-débit, auto, TNT, TNT-HD (idem accents).
+$enreg->emission = 'F2 - journal - 20h -test';
+
+# lancer l'enregistrement.
+$magneto->programmer($enreg);
+
+# lister les enregistrements
+foreach($magneto->lister() as $enreg)
+	echo $enreg;
+	
+?>
